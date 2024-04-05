d22d7b89130b6c226b64febef4cac7589bd9b82b
Pappu Dutta <pappudutta279@gmail.com>
2024-04-05 15:55:47 +0530

    learned to create classes

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+p = Point(2, 8)
+
+print(f"Value of input1 {p.x}")
+print(f"Value of input2 {p.y}")
