792ab10bd1b1eff8551277ebdfaf60133033adc4
Pappu Dutta <pappudutta279@gmail.com>
2024-04-05 15:49:05 +0530

    learned two ways to import functions

--- squares.py
+++ squares.py
@@ -1,5 +1,16 @@
 from functions import square
 
 # For loop
-for i in range(10):
-    print(f"Square of {i} is {square(i)}")
+
+# for i in range(10):
+#     print(f"Square of {i} is {square(i)}")
+
+
+# Other way to import functions (If multiple functions are there import at once)
+
+import functions
+
+# For loop
+
+for i in range(5):
+    print(f"The Square of {i} = {functions.square(i)}")
